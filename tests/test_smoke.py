2587e4f8a58768858e1b97ba14b6b941c09aea68
rvincoy <vin23002@byui.edu>
2025-07-11 20:07:04 -0500

    headless part2

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
-class TestUntitled():
+class TestSmoke():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless=new")
@@ -21,7 +21,7 @@ class TestUntitled():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_untitled(self):
+  def test_smoke(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/")
     self.driver.set_window_size(1200, 800)
     self.driver.find_element(By.LINK_TEXT, "Home").click()
@@ -65,5 +65,5 @@ class TestUntitled():
     self.driver.find_element(By.ID, "password").click()
     self.driver.find_element(By.ID, "password").send_keys("pword")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
+    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
